52b31aafc21c631e370509a5f0c6e2a5dfea2b81
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2020-10-08 00:45:49 +0200

    update the runlasso.m to have smaller thresholds

--- lasso/runlasso.m
+++ lasso/runlasso.m
@@ -5,9 +5,10 @@ prefix="lasso";
 
 ## List of 1-norm constraints to use
 tlist = linspace(0,0.3,21);
+#tlist = [0.1 0.15 0.3 0.5 1 2 3 5 10];
 
 ## Use the maximum coefficients, if available?
-usemaxcoef=1;
+usemaxcoef=0;
 
 #### Do NOT touch past here ####
 if (!exist("octavedump.m","file"))
@@ -15,12 +16,13 @@ if (!exist("octavedump.m","file"))
 endif
 source("octavedump.m");
 
+## scale the ACP terms with the coef0
 for i = 1:columns(x)
   x(:,i) /= coef0(i);
 endfor
 
+## start the loop
 nacp = 0;
-
 printf("#it    t           norm-1       norm-2      norm-inf      wrms    nterm iter\n");
 for it = 1:length(tlist)
   t = tlist(it);
@@ -36,11 +38,12 @@ for it = 1:length(tlist)
     endfor
     clear xtilde
   else
-    [w,iteration] = LassoActiveSet(x,y,t);
+    [w,iteration] = LassoActiveSet(x,y,t,'optTol',1e-9,'zeroThreshold',1e-9);
   endif
 
   worig = w;
 
+  ## unpack the ACP coefficients from the row vector
   nterms = zeros(length(atoms),length(lname));
   n = 0;
   for iat = 1:length(atoms)
@@ -57,6 +60,7 @@ for it = 1:length(tlist)
     endfor
   endfor
 
+  ## Write the ACP in Gaussian format
   if (exist("prefix","var") && !isempty(prefix))
     nacp++;
     fid = fopen(sprintf("%s-%2.2d.acp",prefix,nacp),"w");
@@ -72,10 +76,16 @@ for it = 1:length(tlist)
       endfor
     endfor
     fclose(fid);
+
+    fid = fopen(sprintf("%s-%2.2d.w",prefix,nacp),"w");
+    for i = 1:length(w)
+      fprintf(fid,"%.15e\n",w(i));
+    endfor
+    fclose(fid);
   endif
 
+  ## Calculate the wrms and print the line
   wrms = sqrt(sum((y - x * worig).^2));
-
   printf("%2.2d %.10f %.10f %.10f %.10f %.10f %d %d\n",it,t,...
          sum(abs(w)),sqrt(sum(w.^2)),max(abs(w)),...
          wrms,sum(sum(nterms)),iteration);
